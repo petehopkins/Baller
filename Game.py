cc21240b349699839f9c46e8d1ca71f0fd684f6e
Pete Hopkins <petehopkins@users.noreply.github.com>
2015-11-06 13:45:42 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -6,36 +6,109 @@
 # Requires pygame
 
 import pygame
+from engine import *
+from eventManager import *
+from widgets import *
+from bricks import *
 
-class Game():
-    name = "Baller: Defeat the oppressive war machine of the evil Quadratic invaders!"
-    stageWidth = 800
-    stageHeight = 600
-    stageSize = (stageWidth, stageHeight)
-    stageFillColor = (255, 255, 255)
-    tickSpeed = 60
-    
+class Game(Engine):
+    def __init__(self):
+        super().__init__()
+        self.eventManager = EventManager()
+        self.ticker = Engine.Ticker(self.eventManager, self)
+
+##    def makeWallOBricks(self):
+##        self.window.fill(self.windowFillColor)
+##        wall = Bricks(self.window, 8)
+##        wall.redrawWall(self.window)
+##        return True
+
+    def showSplash(self):
+        pass
+
+    def postNewGameEvent(self):
+        event = Events.NewGameEvent()
+        self.eventManager.post(event)
+
+    def postShowOptionsEvent(self):
+        event = Events.ShowOptionsEvent()
+        self.eventManager.post(event)
+
+    def postQuitEvent(self):
+        event = Events.QuitEvent()
+        self.eventManager.post(event)
+
+    def makeOptions(self):
+        screen = Engine.Layer()
+
+        title = Label(self.eventManager, "Options", self.Colors.BLACK)
+        title.centerOn(self.window)
+        title.setPosition(y = 100)
+        screen.addWidget(title)
+
+        difficulty = Label(self.eventManager, "Difficulty", self.Colors.BLACK)
+        difficulty.setPosition(50, 300)
+        screen.addWidget(difficulty)
+
+        saveButton = Button(self.eventManager, "Save", buttonColor = self.Colors.GREY)
+        saveButton.setPosition(50, 400)
+        screen.addWidget(saveButton)
+
+        cancelButton = Button(self.eventManager, "Cancel", buttonColor = self.Colors.LAVENDER)
+        cancelButton.setPosition(625, 400)
+        screen.addWidget(cancelButton)
+
+        return screen
+
+    def makeStart(self):
+        screen = Engine.Layer()
+
+        title = Label(self.eventManager, self.name, self.Colors.LAVENDER)
+        title.centerOn(self.window)
+        title.setPosition(y = 200)
+        screen.addWidget(title)
+
+        startButton = Button(self.eventManager, "Start", buttonColor = Engine.Colors.GREY, onClickAction = self.postNewGameEvent)
+        startButton.setPosition(50, 500)
+        screen.addWidget(startButton)
+
+        optionsButton = Button(self.eventManager, "Options", onClickAction = self.postShowOptionsEvent)
+        optionsButton.centerOn(self.window)
+        optionsButton.setPosition(y = 500)
+        screen.addWidget(optionsButton)
+
+        quitButton = Button(self.eventManager, "Quit", onClickAction = self.postQuitEvent)
+        quitButton.setPosition(625, 500)
+        screen.addWidget(quitButton)
+
+        return screen
+
+    def showScreen(self, screen):
+        self.window.fill(self.windowFillColor)
+        screen.addWidgetListeners()
+        screen.redrawWidgets(self.window)
+
+    def notify(self, event):
+        if isinstance(event, Events.ShowOptionsEvent):
+            self.screens[self.activeScreen].deactivate()
+            self.activeScreen = "options"
+            self.showScreen(self.screens[self.activeScreen])
+
     @staticmethod
-    def end():
-        pygame.display.quit()
-        pygame.quit()
-    
-    pygame.init()
-    stage = pygame.display.set_mode(stageSize)
-    pygame.display.set_caption(name)
-    clock = pygame.time.Clock()
-    running = True
-    
-    while running:
-        for e in pygame.event.get():
-            if e.type == pygame.QUIT:
-                running = False
-        
-        #pass the variable "stage" to your object's initializer to have access to the screen/window/stage thingy for drawing on it
-        #also, put calls to animate things in this loop
-        
-        pygame.display.flip()
-        clock.tick(tickSpeed)
-        
-Game()
-Game.end()
+    def launch():
+        game = Game()
+
+        event = Events.ShowOptionsEvent()
+        game.eventManager.addListener(event, game)
+
+        screen =game.makeStart()
+        game.screens["start"] = screen
+        game.activeScreen = "start"
+
+        screen = game.makeOptions()
+        game.screens["options"] = screen
+
+        game.showScreen(game.screens[game.activeScreen])
+        game.eventManager.processEvents()
+
+Game.launch()

77c6108ac25b903570fb3e3c69b7f078e42634f0
mjarmand27 <matthew.armand@rockets.utoledo.edu>
2015-11-10 15:46:07 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,37 +1,73 @@
-class Ball():
-    def __init__(self, eventManager):
+import pygame
+from engine import Engine
+from eventManager import Events
+
+class Ball(Engine.GUI.Widget):
+    def __init__(self, eventManager, container = None):
+        super().__init__(eventManager, container)
+        
         self.eventManager
-        self.diameter = 25
-        self.x = 150
-        self.y = 150
-        self.dx = 2
-        self.dy = 2
+        #making rectangular
+        self.width = 25
+        self.height = 25
+        self.xDirection = 1
+        self.yDirection = 1
+        self.dx = 150
+        self.dy = 150
         self.speed = 5
+        self.color = (255,0,0)
+
+        self.image = pygame.Surface((self.width, self.height))
+        self.image.fill(self.color)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = 150
+        self.rect.y = 150
 
     def move(self):
-        self.dx = self.x + self.speed
-        self.dy = self.y + self.speed
-        self.x = self.dx
-        self.y = self.dy
+        self.dx = self.xDirection * self.speed
+        self.dy = self.yDirection * self.speed
         
-    def bounce():
-        if self.x > 800 - self.diameter or self.x < 0:
-            self.dx * -1
-        if self.y < 0:
-            self.dy * -1
+        self.rect.x += self.dx
+        self.rect.y += self.dy
+
+    #if boundaries are hit    
+    #def bounce():
+        if self.rect.x > (800 - self.width) or self.rect.x < 0:
+            self.xDirection *= -1
+        
+        if self.rect.y < 0 or self.rect.y > (600 - self.height):
+            self.yDirection *= -1
+
         #lose life
-        if self.y > (paddle location)
-            self.lives -= 1
+##        if self.y > (paddle location)
+##            return self.lose_life
             
-        for paddle in paddle
-            self.dx * -1
-            self.dy * -1
-            
-    def collision():
-        for brick in brick
-            self.dx * -1
-            self.dy * -1
+    #if paddle/brick is hit        
+##    def collision():
+##        if paddle:
+##            if hit = True
+##            self.dy *= -1
+##            
+##        for brick in bricks
+##            if (hit side) = True
+##            self.dx *= -1
+##            if (hit top/bottom) = True
+##            self.dy *= -1
             #add score?
             #link to destroy brick
 
-    pygame.draw.circle(screen, black, [x, y], diameter)
+    def addListeners(self,):
+        event = Events.TickEvent()
+        self.eventManager.addListener(event, self)
+            
+    def notify(self, event):
+        if isinstance(event, Events.TickEvent):
+            #reposition ball
+            self.move()
+    #call self.eventManager.post(eventType) to throw an event
+
+    #for draw - define surface (rectangle) image property self.image self.rect
+    
+
+

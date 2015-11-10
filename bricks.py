5bdb5b636212cecd260a8b3ed92fc680faafdedc
Pete Hopkins <petehopkins@users.noreply.github.com>
2015-11-10 08:49:52 -0500

    Update bricks.py

--- bricks.py
+++ bricks.py
@@ -2,17 +2,16 @@ import pygame
 from brick import *
 
 class Bricks():
-    __pile = []
-
-    def __init__(self, window, gap = 2):
+    def __init__(self, eventManager, window, gap = 2):
+        self.eventManager = eventManager
         self.__pile = pygame.sprite.Group()
-        self.createWall(window, gap)
+        self.createWall(eventManager, window, gap)
 
     def getPile(self):
         return self.__pile
 
-    def createWall(self, window, gap):
-        brick = Brick([0,0])
+    def createWall(self, eventManager, window, gap):
+        brick = Brick(eventManager, [0,0])
         availableWidth = window.get_width()
         availableHeight = window.get_height() - 200 #allocate 200px on bottom for paddle and maneuvering space
 
@@ -21,20 +20,17 @@ class Bricks():
 
         whitespaceWide = gap + (gap * ballparkWide)
         whitespaceHigh = gap + (gap * ballparkHigh)
-        
+
         bricksWide = (availableWidth - whitespaceWide) // brick.getWidth()
         bricksHigh = (availableHeight - whitespaceHigh) // brick.getHeight()
 
         borderSpace = (availableWidth - (bricksWide * brick.getWidth()) - whitespaceWide) // 2
         leftBorder = borderSpace + gap
         rightBorder = availableWidth - borderSpace - gap
-        
+
         for x in range(0, bricksWide):
             for y in range(0, bricksHigh):
                 position = [leftBorder + gap + ((brick.getWidth() + gap) * x), gap + ((brick.getHeight() + gap) * y)]
-                brick = Brick(position)
+                brick = Brick(self.eventManager, position)
                 self.__pile.add(brick)
-        
-        
-    def redrawWall(self, window):
-        self.__pile.draw(window)
+

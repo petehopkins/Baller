cd8fe9739b34c871b3ee718435d9c756b05eef35
mjarmand27 <matthew.armand@rockets.utoledo.edu>
2015-11-17 23:06:09 -0500

    Update ball.py

--- ball.py
+++ ball.py
@@ -1,12 +1,14 @@
 import pygame
 from engine import Engine
 from eventManager import Events
+from Bricks import *
 
 class Ball(Engine.GUI.Widget):
-    def __init__(self, eventManager, container = None):
-        super().__init__(eventManager, container)
-        
-        self.eventManager
+    def __init__(self, eventManager, stage):
+        super().__init__(eventManager, None)
+
+        self.stage = stage
+        self.eventManager = eventManager
         #making rectangular
         self.width = 25
         self.height = 25
@@ -24,12 +26,25 @@ class Ball(Engine.GUI.Widget):
         self.rect.x = 150
         self.rect.y = 150
 
+    def checkForCollision():
+        #check for boundary
+        #check for paddle
+        #check for bricks
+            #if brick:
+                #brick.collide()
+
+        #if collision:
+            #self.bounce()
     def move(self):
         self.dx = self.xDirection * self.speed
         self.dy = self.yDirection * self.speed
         
         self.rect.x += self.dx
         self.rect.y += self.dy
+
+        #check for boundary collisions
+
+        self.checkForCollisions()
 
     #if boundaries are hit    
     #def bounce():
@@ -38,24 +53,34 @@ class Ball(Engine.GUI.Widget):
         
         if self.rect.y < 0 or self.rect.y > (600 - self.height):
             self.yDirection *= -1
-
-        #lose life
-##        if self.y > (paddle location)
-##            return self.lose_life
-            
+           
     #if paddle/brick is hit        
 ##    def collision():
 ##        if paddle:
 ##            if hit = True
 ##            self.dy *= -1
 ##            
-##        for brick in bricks
-##            if (hit side) = True
-##            self.dx *= -1
-##            if (hit top/bottom) = True
-##            self.dy *= -1
-            #add score?
-            #link to destroy brick
+        spridgets = self.stage.getWidgets()
+        if self in spridgets:
+            spridgets.remove(self)
+        
+        collides = self.groupcollide(self, spridgets)
+        for c in collides:
+            
+                    
+        for brick in bricks
+            if self.isInPlay = True
+                #refer back to brick side/height 
+                #if (leftBorder(self.height in brick?) or rightBorder hit) = True
+                self.dx *= -1
+                self.collide() #call collide function or collision event
+                #refer back to brick top/bottom/width
+                #if (self.width?) = True
+                self.dy *= -1
+                self.collide() #call collide function or collision event?
+                #add score?
+            if self.isInPlay = False
+                pass
 
     def addListeners(self,):
         event = Events.TickEvent()
@@ -65,9 +90,9 @@ class Ball(Engine.GUI.Widget):
         if isinstance(event, Events.TickEvent):
             #reposition ball
             self.move()
-    #call self.eventManager.post(eventType) to throw an event
+
+    self.eventManager.post(eventName) #Collide?
 
     #for draw - define surface (rectangle) image property self.image self.rect
-    
 
 
